0f996d76066ecfa86c0497f7dd91248db43ef690
Алишер Абдукаххаров <aaabdukakhkharov@gmail.com>
2023-07-09 00:53:43 +0500

    Completed third practical task

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,14 +1,29 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from .models import Product, Order
+
 
 def shop_index(request: HttpRequest):
-    products = [
-        ('Laptop', 1999),
-        ('Desktop', 2999),
-        ('Smartphone', 999),
+    menu = [
+        ('products', 'Products in the shop'),
+        ('orders', 'Orders in the shop'),
     ]
     context = {
-        "products": products,
+        "menu": menu,
     }
     return render(request, 'shopapp/shop-index.html', context=context)
+
+
+def products_list(request: HttpRequest):
+    context = {
+        "products": Product.objects.all(),
+    }
+    return render(request, 'shopapp/products-list.html', context=context)
+
+
+def orders_list(request: HttpRequest):
+    context = {
+        "orders": Order.objects.select_related("user").prefetch_related("products").all(),
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)
