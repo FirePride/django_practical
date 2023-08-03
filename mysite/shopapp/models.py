c6c8069b6ece07e6125a303d8ea9887b518c6355
Алишер Абдукаххаров <aaabdukakhkharov@gmail.com>
2023-08-03 18:33:48 +0500

    Completed twelfth practical task

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from myauth.models import Profile
 
 
 class Product(models.Model):
+    class Meta:
+        verbose_name = _("product")
+        verbose_name_plural = _("products")
+
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
@@ -18,6 +23,10 @@ class Product(models.Model):
 
 
 class Order(models.Model):
+    class Meta:
+        verbose_name = _("order")
+        verbose_name_plural = _("orders")
+
     delivery_adress = models.TextField(null=False, blank=True)
     promocode = models.CharField(max_length=20, null=False, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
